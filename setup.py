7cc93cca74317446222a426b8a0b6b8a9f42b2d5
Staciiaz <chollakorn.nimp@gmail.com>
2023-01-23 14:29:50 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="pyftg",
     version="1.0",
-    author="TeamDareFightingICE",
+    author="Staciiaz",
     description="An interface for implementing python AI in DareFightingICE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Staciiaz/pyftg",
     python_requires=">=3.8",
     install_requires=["grpcio"],
     packages=find_packages(where='src'),
