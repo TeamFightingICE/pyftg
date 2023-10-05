d68603b08f5fb9f59f957f3526fc316cf675ea0e
Chollakorn Nimpattanavong <chollakorn.nimp@gmail.com>
2023-10-05 15:57:45 +0900

    release v1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyftg",
-    version="1.1rc1",
+    version="1.1",
     author="Staciiaz",
     description="An interface for implementing python AI in DareFightingICE",
     long_description=long_description,
