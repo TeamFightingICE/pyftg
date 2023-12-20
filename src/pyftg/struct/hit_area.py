04ebaab9e0a83eae8befb0b4bd07b81aa0dcf57e
Chollakorn Nimpattanavong <chollakorn.nimp@gmail.com>
2023-12-20 20:27:57 +0900

    change from tuple to list

--- src/pyftg/struct/hit_area.py
+++ src/pyftg/struct/hit_area.py
@@ -3,5 +3,5 @@ class HitArea:
         self.left: int = hit_area.left
         self.right: int = hit_area.right
         self.top: int = hit_area.top
-        self.bottom: int = hit_area.bottom\
+        self.bottom: int = hit_area.bottom
     
