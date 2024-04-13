12cb49d25b4dc6b0074826d296c6e4c9d4ec304c
Chollakorn Nimpattanavong <chollakorn.nimp@gmail.com>
2024-04-14 04:23:54 +0900

    feat: use socket as default

--- src/pyftg/utils/gateway.py
+++ src/pyftg/utils/gateway.py
@@ -1,10 +1,10 @@
 from pyftg.interfaces.async_gateway import IAsyncGateway
 
 
-def get_async_gateway(host: str, port: int, use_socket=False) -> IAsyncGateway:
-    if use_socket:
-        from pyftg.socket.asyncio.gateway import Gateway as SocketGateway
-        return SocketGateway(host, port)
-    else:
+def get_async_gateway(host: str, port: int, use_grpc: bool = False) -> IAsyncGateway:
+    if use_grpc:
         from pyftg.grpc.asyncio.gateway import Gateway as GrpcGateway
         return GrpcGateway(host, port)
+    else:
+        from pyftg.socket.asyncio.gateway import Gateway as SocketGateway
+        return SocketGateway(host, port)
