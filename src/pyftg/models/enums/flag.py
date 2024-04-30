2d416cb1301a53f95dc5a055c0692c87caff9e0b
Chollakorn Nimpattanavong <chollakorn.nimp@gmail.com>
2024-04-30 23:11:50 +0900

    fix(GrpcFlag): wrong sort order

--- src/pyftg/models/enums/flag.py
+++ src/pyftg/models/enums/flag.py
@@ -4,7 +4,7 @@ from enum import Enum
 class Flag(int, Enum):
     EMPTY = 0
     INITIALIZE = 1
-    PROCESSING = 2
-    ROUND_END = 3
-    GAME_END = 4
-    INIT_ROUND = 5
+    INIT_ROUND = 2
+    PROCESSING = 3
+    ROUND_END = 4
+    GAME_END = 5
